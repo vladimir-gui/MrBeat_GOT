649bb43bbbd6303d6aa2d534c19f8787d4c14233
VladimirGuillochet <vladimir.guillochet@ntymail.com>
2021-09-18 17:48:05 +0200

    add ressources

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from kivy.uix.relativelayout import RelativeLayout
 
 Builder.load_file("track.kv")  # integration du track.kv dans fenetre principale (+voir mrbeat.kv)
 
+NB_TRACKS = 4
+
 
 class MainWidget(RelativeLayout):
-    pass
+    def on_parent(self, widget, parent):
+        """on_parent attend que l'app soit instanciee pour continuer"""
+        pass
 
 
 class MrBeatApp(App):
--- track.py
+++ track.py
@@ -1,5 +1,6 @@
 from kivy.uix.boxlayout import BoxLayout
 
+TRACK_NB_STEPS = 16
 
 class TrackWidget(BoxLayout):
     pass
