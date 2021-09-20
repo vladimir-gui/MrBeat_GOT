4d92546293d1159f5c574bbdf21342cec192e040
VladimirGuillochet <vladimir.guillochet@ntymail.com>
2021-09-20 14:04:14 +0200

    liberation allocation memoire, avant un clic generait un objet = mauvais

--- audio_engine.py
+++ audio_engine.py
@@ -11,7 +11,8 @@ class AudioEngine:
     def __init__(self):
         self.output_stream = get_output(channels=self.NB_CHANNELS, rate=self.SAMPLE_RATE, buffersize=self.BUFFER_SIZE)
 
+        self.audio_source_one_shot = AudioSourceOneShot(self.output_stream)
+        self.audio_source.start()
 
     def play_sound(self, wav_samples):
-        audio_source = AudioSourceOneShot(self.output_stream, wav_samples)
-        audio_source.start()
+        self.audio_source_one_shot.set_wave_samples(wav_samples)
--- audio_source_one_shot.py
+++ audio_source_one_shot.py
@@ -3,20 +3,27 @@ from audiostream.sources.thread import ThreadSource
 
 
 class AudioSourceOneShot(ThreadSource):
-    def __init__(self, output_stream, wav_samples, *args, **kwargs):
+    wav_samples = None
+    nb_wav_samples = 0
+
+    def __init__(self, output_stream, *args, **kwargs):
         ThreadSource.__init__(self, output_stream, *args, **kwargs)
         self.chunk_nb_samples = 32
-        self.wav_samples = wav_samples
-        self.nb_wav_samples = len(wav_samples)
+
         self.current_sample_index = 0
         self.buf = array("h", b"\x00\x00" * self.chunk_nb_samples)  # \x00 equivaut à 0
 
-    def get_bytes(self, *args, **kwargs):
-        for i in range(0, self.chunk_nb_samples):
-            if self.current_sample_index < self.nb_wav_samples:
-                self.buf[i] = self.wav_samples[self.current_sample_index]  # buffer temporaire du chunk
-            else:
-                self.buf[i] = 0
-            self.current_sample_index += 1
+    def set_wave_samples(self, wav_samples):
+        self.wav_samples = wav_samples
+        self.nb_wav_samples = len(wav_samples)
 
-        return self.buf.tostring()
+    def get_bytes(self, *args, **kwargs):
+        if self.nb_wav_samples > 0:
+            for i in range(0, self.chunk_nb_samples):
+                if self.current_sample_index < self.nb_wav_samples:
+                    self.buf[i] = self.wav_samples[self.current_sample_index]  # buffer temporaire du chunk
+                else:
+                    self.buf[i] = 0
+                self.current_sample_index += 1
+
+        return self.buf.tobytes()
