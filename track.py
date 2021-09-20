6ba7b225b4d1bb3117cf5985424ce3b9828651d0
VladimirGuillochet <vladimir.guillochet@ntymail.com>
2021-09-20 09:39:50 +0200

    mise en place Nom du soundkit (SoundKitButton)

--- track.py
+++ track.py
@@ -1,4 +1,5 @@
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 from kivy.uix.togglebutton import ToggleButton
 
 TRACK_NB_STEPS = 16
@@ -8,8 +9,14 @@ class TrackStepButton(ToggleButton):
     pass
 
 
+class SoundKitButton(Button):
+    pass
+
+
 class TrackWidget(BoxLayout):
+    """ structure d'un track """
     def __init__(self, **kwargs):
         super(TrackWidget, self).__init__(**kwargs)
-        for i in range(0, TRACK_NB_STEPS):
+        self.add_widget(SoundKitButton())  # ajout nom du soundkit
+        for i in range(0, TRACK_NB_STEPS):  # ajout des stepbutton
             self.add_widget(TrackStepButton())
