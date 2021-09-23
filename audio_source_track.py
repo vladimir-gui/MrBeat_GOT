93f60ca2069f50a4f56ce312320ad29b2a7bb088
VladimirGuillochet <vladimir.guillochet@ntymail.com>
2021-09-23 14:41:28 +0200

    mise en place methode issue du profiling

--- audio_source_track.py
+++ audio_source_track.py
@@ -17,13 +17,14 @@ class AudioSourceTrack(ThreadSource):
         self.bpm = bpm
         self.sample_rate = sample_rate
         # self.compute_step_nb_samples_and_alloc_buffer()
-        # self.last_sound_sample_start_index = 0
-        self.last_sound_sample_start_index = -self.nb_wav_samples  # FIX V1 : evite de jouer le son au demarrage
+        self.last_sound_sample_start_index = 0
+        # self.last_sound_sample_start_index = -self.nb_wav_samples  # FIX V1 : evite de jouer le son au demarrage
         # self.no_steps_activated() # FIX V2 : evite de jouer le son au demarrage
 
         self.step_nb_samples = self.compute_step_nb_samples(bpm)
         self.buffer_nb_samples = self.compute_step_nb_samples(min_bpm)
         self.buf = array("h", b"\x00\x00" * self.buffer_nb_samples)
+        self.silence = array("h", b"\x00\x00" * self.buffer_nb_samples)
 
     def set_steps(self, steps):
         if not len(steps) == len(self.steps):  # evite le rejeu boucle si changement step
@@ -40,17 +41,26 @@ class AudioSourceTrack(ThreadSource):
             return n
         return 0
 
-    # def no_steps_activated(self):
-    #     """ FIX V2 : evite de jouer le son au demarrage"""
-    #     if len(self.steps) == 0:
-    #         return True  # tous steps desactivés ou aucun step
-    #
-    #     for i in range(0, len(self.steps)):
-    #         if self.steps[i] == 1:
-    #             return False  # au moins un step activé
-    #     return True
+    def no_steps_activated(self):
+        """ FIX V2 : evite de jouer le son au demarrage"""
+        if len(self.steps) == 0:
+            return True  # tous steps desactivés ou aucun step
+
+        for i in range(0, len(self.steps)):
+            if self.steps[i] == 1:
+                return False  # au moins un step activé
+        return True
 
     def get_bytes_array(self):
+        # 1 - Aucun pas d'activé = silence
+        result_buf = None
+        if self.no_steps_activated():
+            result_buf = self.silence[0:self.step_nb_samples]
+        elif self.steps[self.current_step_index] == 1 :
+            # 2 - Step activé et le son à plus de samples que 1 step
+
+
+
         for i in range(0, self.step_nb_samples):
             # if len(self.steps) > 0 and not self.no_steps_activated(): # FIX V2 : evite de jouer le son au demarrage
             if len(self.steps) > 0:
